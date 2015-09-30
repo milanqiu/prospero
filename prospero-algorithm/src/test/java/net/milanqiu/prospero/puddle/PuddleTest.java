82bb7539f4477a9f6738350fe6cd958274a68bb1
Milan Qiu <milanqiu@gmail.com>
2015-09-30 15:28:27 +0800

    Add some test data on Puddle

--- prospero-algorithm/src/test/java/net/milanqiu/prospero/puddle/PuddleTest.java
+++ prospero-algorithm/src/test/java/net/milanqiu/prospero/puddle/PuddleTest.java
@@ -9,9 +9,7 @@ import org.junit.Test;
  */
 public class PuddleTest {
 
-    private static final int TEST_DATA_COUNT = 9;
-
-    private int[][] inputsWallsHeight = new int[][] {
+    private static final int[][] inputsWallsHeight = new int[][] {
             new int[] {},
             new int[] {5, 5},
             new int[] {3, 5, 5, 2},
@@ -21,9 +19,13 @@ public class PuddleTest {
             new int[] {2, 5, 1, 3, 1, 2, 1, 7, 7, 6},
             new int[] {2, 5, 1, 3, 1, 2, 1, 7, 3, 7, 4, 6},
             new int[] {6, 1, 4, 6, 7, 5, 1, 6, 4},
+            new int[] {5, 4, 3, 2, 1},
+            new int[] {5, 4, 3, 2, 1, 4},
+            new int[] {3, 1, 1, 4, 1, 2, 3, 3, 6},
+            new int[] {1, 2, 7, 1, 2, 6, 2, 1, 5, 3, 4, 1, 2, 6, 1, 5, 1, 2},
     };
 
-    private int[] outputsVolume = new int[] {
+    private static final int[] outputsVolume = new int[] {
             0,
             0,
             0,
@@ -33,7 +35,13 @@ public class PuddleTest {
             17,
             23,
             13,
+            0,
+            6,
+            11,
+            38,
     };
+
+    private static final int TEST_DATA_COUNT = inputsWallsHeight.length;
 
     @Test
     public void test_SolutionPerfect() throws Exception {
